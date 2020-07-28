68d8a6ef988839a5c1b6a007148471141f13278a
joshi365 <sumit.joshi.365@gmail.com>
2020-07-28 08:30:32 -0700

    fixed send otp

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     serviceID:"VAa2ff925f3d979f117aeb4fcf8e181105",
     accountSID:"ACe4de5e6574b052bb74cb08526c963828",
-    authToken:"c6f2ddb6b8bf435b8819dc1d3d476cdd"
+    authToken:"edc7f5877cb090ba10d1309769c52d35"
 
 }
