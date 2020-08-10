9f0b5861f3e120c7ed3462a8a08d81c8a9e74292
joshi365 <sumit.joshi.365@gmail.com>
2020-08-10 15:10:22 +0530

    preparing for deploy

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 //Import Routes
 const authRoute = require("./routes/auth");
@@ -25,6 +26,17 @@ app.use(bodyParser.json());
 app.use("/api/user", authRoute);
 app.use("/api/music", musicRoute);
 app.use("/api/profile", profileRoute);
+
+//SERVER static assets if in production
+
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
